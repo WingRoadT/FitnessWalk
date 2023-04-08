50790c93ca22f2a1869fba79493366395863ae57
WingRoadT <126765335+WingRoadT@users.noreply.github.com>
2023-04-08 17:07:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,16 +5,19 @@ window.onload = function() {
         document.getElementById("topic").innerHTML = "Hong Kong Fitness Walking Tracks";
         document.getElementById("content").innerHTML = "Language: ";
         document.getElementById("back").innerHTML = "English";
+        document.getElementById("searchtext").innerHTML = "Search";
     } 
     else if(buttonId == "tc"){
         document.getElementById("topic").innerHTML = "香港健步行";
         document.getElementById("content").innerHTML = "語言: ";
         document.getElementById("back").innerHTML = "繁體中文";
+        document.getElementById("searchtext").innerHTML = "搜尋";
     }
     else {
         document.getElementById("topic").innerHTML = "香港健步行";
         document.getElementById("content").innerHTML = "语言: ";
         document.getElementById("back").innerHTML = "简体中文";
+        document.getElementById("searchtext").innerHTML = "搜寻";
     }
 
     fetch('data.json')
@@ -54,6 +57,20 @@ window.onload = function() {
 }
 var urlParams = new URLSearchParams(window.location.search);
 var buttonId = urlParams.get('id');
+
+function searchJson() {
+    var searchValue = document.getElementById("searchInput").value;
+    if(buttonId == "en"){
+        window.location.href = "result.html?id=en&search=" + searchValue;
+    }
+    else if(buttonId == "tc"){
+        window.location.href = "result.html?id=tc&search=" + searchValue;
+    }
+    else {
+        window.location.href = "result.html?id=sc&search=" + searchValue;
+    }
+}
+  
 function detail(place){
     if(buttonId == "en"){
         window.location.href = "detail.html?id=en&place=" + place;
